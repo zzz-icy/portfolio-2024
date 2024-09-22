0e068233f5dd1b3ca7ac7c19e7ae2c0b47b20323
Hannah Z <eveline1129@163.com>
2024-09-22 01:49:42 -0500

    added me picture

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,26 +1,37 @@
 import React from "react"
 import "./About.css"
 import GithubCommits from "../../assets/images/github-commits.png"
+import Me from "../../assets/images/me.jpg"
+
 const About = () => {
 	return (
 		<section
 			className='about'
 			id='about'
 		>
-			<p>
-				Hey there, I'm <strong>Hannah</strong>! <br />
-				<br /> I’m all about crafting sleek, intuitive UIs and creating seamless
-				user experiences that just <em>feel</em> right. <br />
-				<br />
-				Whether it's solving design puzzles or coding up something fresh, I’m
-				always chasing that perfect balance of form and function. <br />
-				<br />
-				Oh, and when I’m not deep in design mode, I’m juggling life as a mom to
-				a tiny human&#x1F476; (3 months old!) and a pretty chill cat&#x1F63A;.
-				<br />
-				<br />
-				Let’s make something awesome together!
-			</p>
+			<div className='about-container'>
+				<p>
+					Hey there, I'm <strong>Hannah</strong>! <br />
+					<br /> I’m all about crafting sleek, intuitive UIs and creating
+					seamless user experiences that just <em>feel</em> right. <br />
+					<br />
+					Whether it's solving design puzzles or coding up something fresh, I’m
+					always chasing that perfect balance of form and function. <br />
+					<br />
+					Oh, and when I’m not deep in design mode, I’m juggling life as a mom
+					to a tiny human&#x1F476; (3 months old!) and a pretty chill
+					cat&#x1F63A;.
+					<br />
+					<br />
+					Let’s make something awesome together!
+				</p>
+				<img
+					src={Me}
+					className='me'
+					alt='GithubCommits'
+				/>
+			</div>
+
 			<img
 				src={GithubCommits}
 				className='github'
